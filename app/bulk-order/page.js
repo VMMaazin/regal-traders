441fecbb9dad33e79884d05fe9ac35601b80f0f5
MAAZIN <maazinmuhammed0@gmail.com>
2026-02-14 18:49:04 +0530

    Test new route

--- app/bulk-order/page.js
+++
@@ -1,8 +0,0 @@
-export default function BulkOrderPage() {
-    return (
-        <div style={{ padding: '100px', textAlign: 'center' }}>
-            <h1>Bulk Order Page</h1>
-            <p>If you can see this, the page is working!</p>
-        </div>
-    )
-}
---
+++ app/order-inquiry/page.js
@@ -0,0 +1,8 @@
+export default function OrderInquiryPage() {
+    return (
+        <div style={{ padding: '100px', textAlign: 'center' }}>
+            <h1>Order Inquiry Page</h1>
+            <p>This is a test!</p>
+        </div>
+    )
+}
