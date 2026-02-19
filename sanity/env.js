7cdfe96d5c6fcdb96a8a3184a8e2cc74a04e4535
MAAZIN <maazinmuhammed0@gmail.com>
2026-02-19 16:46:43 +0530

    sanity studio updated

--- sanity/env.js
+++ sanity/env.js
@@ -1,5 +1,4 @@
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-02-19'
+export const apiVersion = '2026-02-19'
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const dataset = 'production'
+export const projectId = '7o6slva4'
